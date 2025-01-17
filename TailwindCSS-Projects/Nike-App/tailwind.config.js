89e908a78bc5518b99654affe5705fa01e1c7444
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-01-17 18:48:51 +0530

    Adding own styling components for more flexibility and readability of code

--- TailwindCSS-Projects/Nike-App/tailwind.config.js
+++ TailwindCSS-Projects/Nike-App/tailwind.config.js
@@ -1,10 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode : "class",
   content: ["./*.html",
             "./*.html"],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '58px'],
+      '8xl': ['96px', '106px']
+    },
+    extend: {
+      colors: {
+        'chestnut-100': '#973f2922',
+        'chestnut-200': '#973f2944',
+        'chestnut-300': '#973f2988',
+        'chestnut-400': '#973f2999'
+      },
+      fontFamily: {
+        palanquin: ['Palanquin', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'primary': "#ECEEFF",
+        "coral-red": "#FF6452",
+        "slate-gray": "#6D6D6D",
+        "pale-blue": "#F5F6FF",
+        "white-400": "rgba(255, 255, 255, 0.80)"
+      },
+      boxShadow: {
+        '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
+      },
+      backgroundImage: {
+        'hero': "url('Assets/images/collection-background.svg')",
+        'card': "url('Assets/images/thumbnail-background.svg')",
+      },
+      screens: {
+        "wide": "1440px"
+      }
+    },
   },
   plugins: [],
 }
-
