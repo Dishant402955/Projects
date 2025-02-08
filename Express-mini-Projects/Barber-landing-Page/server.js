64c31c5a41903c43fca9fb9e5929fc9bb55660a3
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-02-08 10:05:38 +0530

    Created 2 single page landing pages and served it statically with express server

---
+++ Express-mini-Projects/Barber-landing-Page/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "./public/index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server is up and listening ~~~");
+});
